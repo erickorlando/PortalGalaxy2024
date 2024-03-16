28f902f79dc1f6fb5581b868811da696b3e02493
Erick Orlando <erickorlando@outlook.com>
2024-03-16 14:05:51 -0500

    Serilog

--- PortalGalaxy/Server/Program.cs
+++ PortalGalaxy/Server/Program.cs
@@ -5,13 +5,42 @@ using Microsoft.IdentityModel.Tokens;
 using PortalGalaxy.DataAccess;
 using PortalGalaxy.Repositories.Interfaces;
 using PortalGalaxy.Services.Interfaces;
+using PortalGalaxy.Services.Profiles;
 using PortalGalaxy.Shared.Configuracion;
 using Scrutor;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
 using System.Text;
-using Microsoft.EntityFrameworkCore.Diagnostics;
-using PortalGalaxy.Services.Profiles;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuracion de Logs
+var logger = new LoggerConfiguration()
+    .WriteTo.MSSqlServer(builder.Configuration.GetConnectionString("PortalGalaxy"),
+        new MSSqlServerSinkOptions
+        {
+            AutoCreateSqlDatabase = true,
+            AutoCreateSqlTable = true,
+            TableName = "GalaxyLogs"
+        }, restrictedToMinimumLevel: LogEventLevel.Warning)
+    .CreateLogger();
+
+// Seteamos a Serilog como proveedor de Logs
+builder.Logging.AddSerilog(logger);
+
+if (builder.Environment.IsDevelopment())
+{
+    var loggerFile = new LoggerConfiguration()
+        .WriteTo.Console(LogEventLevel.Information)
+        .WriteTo.File("..\\registro.log",
+            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] - {Message}{NewLine}{Exception}",
+            rollingInterval: RollingInterval.Day,
+            restrictedToMinimumLevel: LogEventLevel.Warning)
+        .CreateLogger();
+
+    builder.Logging.AddSerilog(loggerFile);
+}
 
 // Mapeamos el contenido del archivo de configuracion en una clase fuertemente tipada
 builder.Services.Configure<AppSettings>(builder.Configuration);
