76188d94b2a55449148619de2650db223f3bc2f5
Erick Orlando <erickorlando@outlook.com>
2024-03-23 11:44:23 -0500

    Exportacion a Excel

---
+++ PortalGalaxy/Client/Xls/PlantillaXls.cs
@@ -0,0 +1,19 @@
+﻿using ClosedXML.Report;
+
+namespace PortalGalaxy.Client.Xls;
+
+public class PlantillaXls
+{
+    public byte[] GenerarPlantilla(Stream plantilla, object data, string variable = "Talleres")
+    {
+        var template = new XLTemplate(plantilla);
+
+        template.AddVariable(variable, data);
+        template.Generate();
+
+        using var ms = new MemoryStream();
+        template.SaveAs(ms);
+
+        return ms.ToArray();
+    }
+}
